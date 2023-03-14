ae8f92cccdf32a248c65672971fd8d570dc31269
me-cafebabe <me.cafebabe@gmail.com>
2023-03-15 01:15:53 +0800

    enable navbar

--- libinit/init_xiaomi_mi8937.cpp
+++ libinit/init_xiaomi_mi8937.cpp
@@ -119,4 +119,5 @@ void vendor_load_properties() {
     property_override("ro.adb.secure", "0");
     property_override("ro.debuggable", "1");
     property_override("persist.sys.usb.config", "adb");
+    property_override("qemu.hw.mainkeys", "0");
 }
